c63cae72582972ba66b32f1135398fcef056a811
SanidhyaIT <19sanidhyarawat@gmail.com>
2024-08-30 13:15:32 +0530

    new change

--- src/App.js
+++ src/App.js
@@ -6,15 +6,14 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>React Deploy AWS</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
+        
           Learn React with me
         </a>
       </header>
